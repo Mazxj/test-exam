8766916307873651e83918428e2b286b5c033301
Mazxj <335719788@qq.com>
2020-07-01 21:03:21 +0800

    cx

---
+++ test/2017441411_陈旭.java
@@ -0,0 +1,31 @@
+package com.cx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@RestController
+@SpringBootApplication
+
+public class Application {
+	@RequestMapping("/hello")
+	public String home() {
+		return "helllo world";
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+	return new RestTemplate() ;
+	}
+	
+	public static void main(String[] args){
+		SpringApplication.run(Application.class, args);
+	}
+}
